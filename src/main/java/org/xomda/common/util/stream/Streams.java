2fc6dfeb12b88034668b09f1da470dce98334616
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-03-17 00:23:24 +0100

    cascading streams

--- src/main/java/org/xomda/common/util/stream/Streams.java
+++ src/main/java/org/xomda/common/util/stream/Streams.java
@@ -1,6 +1,15 @@
 package org.xomda.common.util.stream;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * Utils that provide functionality for when working with {@link java.util.stream.Stream streams}.
@@ -45,6 +54,67 @@ public final class Streams {
 				: Stream.of(streams)
 						.reduce(Stream::concat)
 						.orElseGet(Stream::empty);
+	}
+
+	/**
+	 * Short way to create a {@link Stream stream} out of an {@link Iterator iterator},
+	 * while still specifying whether it has to be a parallel stream or not.
+	 */
+	public static <T> Stream<T> stream(Iterator<T> it, boolean parallel) {
+		return StreamSupport.stream(Spliterators.spliteratorUnknownSize(it, Spliterator.ORDERED), parallel);
+	}
+
+	/**
+	 * Short way to create a sequential {@link Stream stream} out of an {@link Iterator iterator}.
+	 */
+	public static <T> Stream<T> stream(Iterator<T> it) {
+		return stream(it, false);
+	}
+
+	/**
+	 * Create a cascading stream, starting with the first element,
+	 * proceeding with the outcome of the "next" {@link UnaryOperator unary operator}.
+	 * It will run as long as the returned value isn't null, or the parent itself.
+	 */
+	public static <T> Stream<T> cascading(T start, UnaryOperator<T> next) {
+		return stream(new CascadingIterator<>(start, next));
+	}
+
+	/**
+	 * Create a cascading stream, starting with the first element,
+	 * proceeding with the outcome of the "next" {@link UnaryOperator unary operator}.
+	 * It will run as long as the returned optional isn't empty.
+	 */
+	public static <T> Stream<T> cascading(T start, Function<T, Optional<T>> next) {
+		return stream(new CascadingIterator<>(start, v -> next.apply(v).orElse(null)));
+	}
+
+	// Helper class for creating s cascading stream
+	private static class CascadingIterator<T> implements Iterator<T> {
+		private final AtomicReference<T> current = new AtomicReference<>();
+		private final UnaryOperator<T> next;
+
+		CascadingIterator(T start, UnaryOperator<T> next) {
+			this.next = next;
+			current.set(start);
+		}
+
+		@Override
+		public synchronized boolean hasNext() {
+			return current.get() != null;
+		}
+
+		@Override
+		public synchronized T next() {
+			T result = current.getAndUpdate(next);
+			if (null == result) {
+				throw new NoSuchElementException();
+			}
+			if (result == current.get()) {
+				current.set(null);
+			}
+			return result;
+		}
 	}
 
 	private Streams() {
